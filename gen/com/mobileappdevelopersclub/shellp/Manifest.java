4cf299b61226449aed220ebe476d40572402873d
ely <elysham13@gmail.com>
2013-09-17 19:14:24 -0400

    message

--- gen/com/mobileappdevelopersclub/shellp/Manifest.java
+++ gen/com/mobileappdevelopersclub/shellp/Manifest.java
@@ -9,6 +9,6 @@ package com.mobileappdevelopersclub.shellp;
 
 public final class Manifest {
     public static final class permission {
-        public static final String MAPS_RECEIVE="com.mobileappdevelopersclub.shellp.ui.permission.MAPS_RECEIVE";
+        public static final String MAPS_RECEIVE="com.mobileappdevelopersclub.shellp.permission.MAPS_RECEIVE";
     }
 }
