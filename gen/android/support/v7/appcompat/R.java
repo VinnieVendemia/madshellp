67afcef12416c60f709af3f5db4e250bd8c41870
Vinnie Vendemia <vvendemia1@gmail.com>
2014-01-05 16:56:11 -0500

    most recent update

--- gen/android/support/v7/appcompat/R.java
+++ gen/android/support/v7/appcompat/R.java
@@ -132,7 +132,7 @@ public final class R {
 		public static final int abc_split_action_bar_is_narrow = 0x7f0a0002;
 	}
 	public static final class color {
-		public static final int abc_search_url_text_holo = 0x7f060015;
+		public static final int abc_search_url_text_holo = 0x7f060016;
 		public static final int abc_search_url_text_normal = 0x7f06000a;
 		public static final int abc_search_url_text_pressed = 0x7f06000c;
 		public static final int abc_search_url_text_selected = 0x7f06000b;
